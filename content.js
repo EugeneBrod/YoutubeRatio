874fee0690a733a6a440417d3421c97809dd222d
EugeneBrod <eugenebrod@gmail.com>
2021-11-12 14:42:23 -0800

    version 0.0.0.1 release - version 0.0.0.2 goals: Consistency across all youtube themes, create popup page with link to socials, disable for livestreams, clean up code, improve UI

--- content.js
+++ content.js
@@ -1,65 +1,62 @@
-window.addEventListener ("load", myMain, false);
-
 function myMain (evt) {
 
-  //const title_class = "ytd-video-primary-info-renderer";
-  //const title_class = "style-scope";
+  var loop = function () {
 
-  const title_class = "h1.title.style-scope.ytd-video-primary-info-renderer";
-  var $title = title_class;
-  $title = $title.prev()
-  console.log($title.text())
-
-  loop();
-  //observeDOM(element, myMain)
-}
-
-// https://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
-var observeDOM = (function(){
-  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-  return function( obj, callback ){
-    if( !obj || obj.nodeType !== 1 ) return; 
-
-    if( MutationObserver ){
-      // define a new observer
-      var mutationObserver = new MutationObserver(callback)
-
-      // have the observer observe foo for changes in children
-      mutationObserver.observe( obj, { childList:true, subtree:true })
-      return mutationObserver
-    }
+      // extract number of likes
+      const like_selector = "yt-formatted-string#text.ytd-toggle-button-renderer"
+      var likes = $(like_selector)
+      likesString = likes[0].innerHTML    
+      numberOfLikes = likesString.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; })[0];
+      if (!isNaN(numberOfLikes[numberOfLikes.length-1])) {
+        numberOfLikes = parseInt(numberOfLikes)
+      } else {
+        if (numberOfLikes[numberOfLikes.length-1] == "K") {
+          numberOfLikes = parseInt(numberOfLikes.slice(0,numberOfLikes.length-1)) * 1000
+        }
+        if (numberOfLikes[numberOfLikes.length-1] == "M") {
+          numberOfLikes = parseInt(numberOfLikes.slice(0,numberOfLikes.length-1)) * 1000000
+        }
+      }
     
-    // browser support fallback
-    else if( window.addEventListener ){
-      obj.addEventListener('DOMNodeInserted', callback, false)
-      obj.addEventListener('DOMNodeRemoved', callback, false)
-    }
-  }
-})()
+      //extract number of views
+      const view_selector = "#container > #info > #info-text > #count"
+      var views = $(view_selector)
+      views = views.find("span.view-count")
+      viewsString = views.get()[0].innerHTML
+      numberOfViews = parseInt(viewsString.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; })[0].replaceAll(',',''));
 
-var loop = function () {
-  var likes = document.getElementsByClassName("yt-simple-endpoint style-scope ytd-toggle-button-renderer")
-  for (i=0;i<likes.length;i++) {
-    if (!isNaN(parseInt(likes[i].innerText[0]))) {
-      likesString = likes[i].innerText
+      // calculate and render ratio
+      const ratio = (numberOfLikes/numberOfViews) * 100;
+      if ((!isNaN(ratio))) {
+        likes[1].innerHTML = "likes/views: " + String(ratio).slice(0, 4) + "%";
+      }
+      /* var dislikes = document.getElementsByClassName("style-scope ytd-toggle-button-renderer style-text");
+      dislikes[3].innerHTML = "likes/views: " + String(ratio).slice(0, 4) + "%"; */
     }
-  }
-  var views = document.getElementsByClassName("view-count style-scope ytd-video-view-count-renderer")
-  viewsString = views[0].innerHTML
-  numberOfLikes = likesString.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; })[0];
-  if (!isNaN(numberOfLikes[numberOfLikes.length-1])) {
-    numberOfLikes = parseInt(numberOfLikes)
-  } else {
-    if (numberOfLikes[numberOfLikes.length-1] == "K") {
-      numberOfLikes = parseInt(numberOfLikes.slice(0,numberOfLikes.length-1)) * 1000
-    }
-    if (numberOfLikes[numberOfLikes.length-1] == "M") {
-      numberOfLikes = parseInt(numberOfLikes.slice(0,numberOfLikes.length-1)) * 1000000
-    }
-  }
-  numberOfViews = parseInt(viewsString.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; })[0].replaceAll(',',''));
-  const ratio = (numberOfLikes/numberOfViews) * 100;
-  var dislikes = document.getElementsByClassName("style-scope ytd-toggle-button-renderer style-text");
-  dislikes[3].innerHTML = "likes/views: " + String(ratio).slice(0, 4) + "%";
+  loop();
+
+  // Options for the observer (which mutations to observe)
+  var config = { attributes: true, childList: true };
+
+  // Callback function to execute when mutations are observed
+  var callback = function(mutationsList) {
+      loop();
+  };
+
+  // Create an observer instance linked to the callback function
+  var observer = new MutationObserver(callback);
+
+  const title_class = "#container > h1 > yt-formatted-string";
+  var title = $(title_class);
+
+  var targetNode = title[0];
+    // Select the node that will be observed for mutations
+  //var targetNode = document.getElementById('some-id');
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+
 }
+
+window.addEventListener ("load", myMain, false);
+

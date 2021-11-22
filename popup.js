c0638bce32d5b83a7444c6a627a71ba23da58fb9
EugeneBrod <eugenebrod@gmail.com>
2021-11-22 14:56:40 -0800

    Version 0.0.0.4 release.

--- popup.js
+++
@@ -1,6 +0,0 @@
-var button = document.getElementById('Button')
-button.onclick = function() {
-  var counter = document.getElementById("Counter")
-  var number = parseInt(counter.innerHTML)
-  counter.innerHTML = String(number+1)
-}
